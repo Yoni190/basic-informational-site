0cad4d847d4f3e4d12cfdf8ee653a79ed4811866
Yoni190 <yfisuhu@gmail.com>
2026-03-04 14:17:38 +0300

    Use port defined in env

--- index.js
+++ index.js
@@ -1,36 +1,58 @@
 const http = require('http')
 const fs = require('fs/promises')
+const express = require('express')
+const app = express()
 
-async function main() {
-    try {
-        const index = await fs.readFile('index.html', { encoding: 'utf8' })
-        const about = await fs.readFile('about.html', { encoding: 'utf8' })
-        const contact = await fs.readFile('contact-me.html', { encoding: 'utf8' })
-        const notFound = await fs.readFile('404.html', { encoding: 'utf8' })
+// Without Express
+// async function main() {
+//     try {
+//         const index = await fs.readFile('index.html', { encoding: 'utf8' })
+//         const about = await fs.readFile('about.html', { encoding: 'utf8' })
+//         const contact = await fs.readFile('contact-me.html', { encoding: 'utf8' })
+//         const notFound = await fs.readFile('404.html', { encoding: 'utf8' })
 
-        let pageContent = ''
-        http.createServer((req, res) => {
-            if(req.url == '/') {
-                pageContent = index
-            } 
-            else if(req.url.endsWith('/about')) {
-                pageContent = about
-            }
-            else if(req.url.endsWith('/contact-me')) {
-                pageContent = contact
-            }
-            else {
-                pageContent = notFound
-            }
-            res.statusCode = 200
-            res.write(pageContent)
-            res.end()
-        }).listen(8080, () => {
-            console.log('Server running on port 8080')
-        })
-    } catch (error) {
-        console.log(error)
+//         let pageContent = ''
+//         http.createServer((req, res) => {
+//             if(req.url == '/') {
+//                 pageContent = index
+//             } 
+//             else if(req.url.endsWith('/about')) {
+//                 pageContent = about
+//             }
+//             else if(req.url.endsWith('/contact-me')) {
+//                 pageContent = contact
+//             }
+//             else {
+//                 pageContent = notFound
+//             }
+//             res.statusCode = 200
+//             res.write(pageContent)
+//             res.end()
+//         }).listen(8080, () => {
+//             console.log('Server running on port 8080')
+//         })
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+
+// main()
+
+const index = await fs.readFile('index.html', { encoding: 'utf8' })
+const about = await fs.readFile('about.html', { encoding: 'utf8' })
+const contact = await fs.readFile('contact-me.html', { encoding: 'utf8' })
+const notFound = await fs.readFile('404.html', { encoding: 'utf8' })
+
+const PORT = process.env.PORT || 8080
+
+app.get('/', (req, res) => {
+    res.send(index)
+})
+
+app.listen(PORT, (error) => {
+    if(error) {
+        throw error
     }
-}
 
-main()
+    console.log(`Server listening on port ${PORT}`)
+})
